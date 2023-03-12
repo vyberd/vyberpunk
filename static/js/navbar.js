707d6529d31f2242de3370d636056e286346f4f3
Gergo Vari <work@varigergo.hu>
2023-03-12 14:40:18 +0100

    fix: csp navbar

---
+++ static/js/navbar.js
@@ -0,0 +1,6 @@
+const toggleBurger = () => {
+	let burgerIcon = document.getElementById('burger');
+	let dropMenu = document.getElementById('navbar');
+	burgerIcon.classList.toggle('is-active');
+	dropMenu.classList.toggle('is-active');
+};
