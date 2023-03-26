ff11b8b3b09c56a220b1e38994ec5ca20b20550c
Gergo Vari <work@varigergo.hu>
2023-03-26 15:51:34 +0200

    feat: cookie banner WIP

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,12 +1,11 @@
-function isVisible(element, className) {
+function isClassOn(element, className) {
 	if (element) {
 		return element.className.includes(className);
 	} else {
 		console.log("isVisible: Non-existent element!")
 	}
 }
-
-function isVisibleByName(name, className) {
+function isClassOnByName(name, className) {
 	let element = document.getElementById(name);
 	if (element) {
 		return element.className.includes(className);
@@ -15,55 +14,90 @@ function isVisibleByName(name, className) {
 		return false;
 	}
 }
-
 function toggleClass(element, className) {
 	if (element) {
-		element.className = isVisible(element, className) ? element.className.replace(" " + className, "") : element.className + " " + className;
-		return isVisible(element, className);
+		element.className = isClassOn(element, className) ? element.className.replace(" " + className, "") : element.className + " " + className;
+		return isClassOn(element, className);
 	} else {
 		console.log("toggleClass: Non-existent element!");
 		return false;
 	}
 }
-
 function togglePopupClass(popup) {
 	return toggleClass(popup, "popup-visible");
 }
 function toggleScroll() {
 	return toggleClass(document.documentElement, "no-scroll");
 }
-
 function togglePopup(name) {
 	if (name) {
-		toggleScroll();
-		togglePopupClass(document.getElementById(name))
+		let element = document.getElementById(name);
+		if (isClassOn(element, "popup-no-scroll")) {
+			toggleScroll();
+		}
+		togglePopupClass(element)
 	} else {
 		console.log("showPopup: You need to provide a name!");
 	}
 }
 
-var isPopupShotAtExit = false;
-document.addEventListener('mouseout', e => {
-    if (!e.toElement 
-	&& !e.relatedTarget 
-	&& !isPopupShotAtExit 
-	&& !isVisibleByName('popup-newsletter', "popup-visible")) {
-	togglePopup("popup-newsletter");
-	isPopupShotAtExit = true;
-    }
-});
-
-var isPopupShotAtEnd = false;
-window.onscroll = function(ev) {
-	if ((window.innerHeight + Math.ceil(window.pageYOffset)) >= document.body.offsetHeight - 2) {
-		if (!isPopupShotAtEnd 
-		&& !isVisibleByName('popup-newsletter', "popup-visible")) {
-			togglePopup("popup-newsletter");
-			isPopupShotAtEnd = true;
-		}
+function addExitTrigger(elem) {
+	if (elem) {
+		elem.onclick = function() { 
+			togglePopup(elem.id.replace("-exit", "")); 
+		};
+	} else {
+		console.log("addExitTrigger: Non-existent element!");
 	}
-};
+}
+function addExitTriggerAll() {
+	for (const elem of document.querySelectorAll('[id^="popup-"][id$="-exit"]')) {
+		addExitTrigger(elem);
+	}
+}
 
-document.getElementById("popup-newsletter-exit").onclick = function() { 
-	togglePopup("popup-newsletter"); 
-};
+function addOnLeaveIntent(name) {
+	document.addEventListener('mouseout', e => {
+	    if (!e.toElement 
+		&& !e.relatedTarget 
+		&& !isClassOnByName(name, "popup-shot-at-end") 
+		&& !isClassOnByName(name, "popup-visible")) {
+		togglePopup(name);
+		toggleClass(document.getElementById(name), "popup-shot-at-end");
+	    }
+	});
+}
+function addOnLeaveIntentAll() {
+	for (const elem of document.querySelectorAll(".popup-before-exit")) {
+		addOnLeaveIntent(elem.id);
+	}
+}
+
+function addOnEnd(name) {
+	window.onscroll = function(ev) {
+		if ((window.innerHeight + Math.ceil(window.pageYOffset)) >= document.body.offsetHeight - 2) {
+			if (
+			   !isClassOnByName(name, "popup-shot-before-exit") 
+			&& !isClassOnByName(name, "popup-visible")) {
+				togglePopup(name);
+				toggleClass(document.getElementById(name), "popup-shot-before-exit");
+			}
+		}
+	};
+}
+function addOnEndAll() {
+	for (const elem of document.querySelectorAll(".popup-at-end")) {
+		addOnEnd(elem.id);
+	}
+}
+
+function handleStartPopups() {
+	for (const elem of document.querySelectorAll(".popup-on")) {
+		togglePopup(elem.id);
+	}
+}
+
+addExitTriggerAll();
+addOnLeaveIntentAll();
+addOnEndAll();
+handleStartPopups();
