f5d55c5559376a2dbf97fd42b0b98ba4aa6cf577
Gergo Vari <work@varigergo.hu>
2023-04-11 14:04:24 +0200

    refactor

---
+++ assets/js/gallery.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", function () {
+	const swiper = new Swiper('.swiper', {
+		slidesPerView: 3,
+		spaceBetween: 20,
+		pagination: {
+			el: '.swiper-pagination',
+		},
+	});
+});
