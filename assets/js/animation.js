bc8d1eb6b6edb38121242dbf708e71be86a750f6
Gergo Vari <work@varigergo.hu>
2023-04-11 18:23:14 +0200

    feat: animations

---
+++ assets/js/animation.js
@@ -0,0 +1,18 @@
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("animate-slidein");
+		}
+	});
+});
+
+function setupObserver() {
+	document.querySelectorAll('h1, .stack-offer').forEach(elem => {
+		observer.observe(elem);
+	});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	setupObserver();
+});
+setupObserver();
