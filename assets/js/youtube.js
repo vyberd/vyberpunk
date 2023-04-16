341fa4aea9bd6ffc97ac494cb8882a91147932b4
Gergo Vari <work@varigergo.hu>
2023-04-16 10:24:34 +0200

    fix: yt

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -1,13 +1,5 @@
 var youtube = document.querySelectorAll(".youtube");
 for (var i = 0; i < youtube.length; i++) {
-	var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.id +"/maxresdefault.jpg";
-	var image = new Image();
-	image.src = source;
-	image.addEventListener("load", 
-		function() {
-			youtube[i].appendChild(image);
-		}(i) 
-	);
 	youtube[i].addEventListener("click", function() {
 		var iframe = document.createElement("iframe");
 		iframe.setAttribute("frameborder", "0");
