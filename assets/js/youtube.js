a29a7526f87210ad3dc579cdd7da3d19fe519ea9
Gergo Vari <work@varigergo.hu>
2023-04-16 11:10:13 +0200

    fix

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -1,12 +1,16 @@
-var youtube = document.querySelectorAll(".youtube");
-for (var i = 0; i < youtube.length; i++) {
-	youtube[i].addEventListener("click", function() {
-		var iframe = document.createElement("iframe");
-		iframe.setAttribute("frameborder", "0");
-		iframe.setAttribute("allowfullscreen", "");
-		iframe.setAttribute("allow", "autoplay");
-		iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + this.dataset.id + "?rel=0&showinfo=0&autoplay=1");
-		this.innerHTML = "";
-		this.appendChild(iframe);
-	});	
-};
+function initYoutube() {
+	var youtube = document.querySelectorAll(".youtube");
+	for (var i = 0; i < youtube.length; i++) {
+		youtube[i].addEventListener("click", function() {
+			var iframe = document.createElement("iframe");
+			iframe.setAttribute("frameborder", "0");
+			iframe.setAttribute("allowfullscreen", "");
+			iframe.setAttribute("allow", "autoplay");
+			iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + this.dataset.id + "?rel=0&showinfo=0&autoplay=1");
+			this.innerHTML = "";
+			this.appendChild(iframe);
+		});	
+	};
+}
+document.addEventListener("DOMContentLoaded", initYoutube);
+initYoutube();
