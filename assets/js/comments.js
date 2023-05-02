a821ba9189d7ec36c3d6b09d1cf844e75e724300
Gergo Vari <work@varigergo.hu>
2023-05-02 17:25:50 +0200

    refactor: move to blog module

--- assets/js/comments.js
+++
@@ -1,9 +0,0 @@
-var remark_config = {
-	host: 'https://comments.varigergo.hu',
-	site_id: 'blog.varigergo.hu',
-	components: ['embed'],
-	no_footer: true,
-	theme: 'dark',
-	locale: 'en'
-}
-!function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);
--- content/daily/_index.md
+++
@@ -1,3 +0,0 @@
----
-title: "Napi e-mailek"
----
