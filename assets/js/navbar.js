ce377dcb5c423ee18c69747a893c1d2ca2973524
Gergo Vari <work@varigergo.hu>
2023-05-08 15:20:03 +0200

    refactor

--- assets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-const toggleBurger = () => {
-	let burgerIcon = document.getElementById('burger');
-	let dropMenu = document.getElementById('navbar');
-	burgerIcon.classList.toggle('is-active');
-	dropMenu.classList.toggle('is-active');
-};
-
-document.getElementById("burger").onclick = toggleBurger;
